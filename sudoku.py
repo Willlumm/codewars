0caa4faee3de196a519d9fe428bda94293c27f09
Willlumm <william.lumme@gmail.com>
2023-07-20 00:27:17 +0100

    Refactor sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,15 @@
 # https://www.codewars.com/kata/5296bc77afba8baa690002d7/train/python
 
 def get_row(puzzle, y):
-    row = puzzle[y]
-    return row
+    return puzzle[y]
 
 def get_col(puzzle, x):
-    col = list(list(zip(*puzzle))[x])
-    return col
+    return list(list(zip(*puzzle))[x])
 
 def get_square(puzzle, x, y):
     x_min = (x // 3) * 3
     y_min = (y // 3) * 3
-    square = [pos for row in puzzle[y_min : y_min+3] for pos in row[x_min : x_min+3]]
-    return square
+    return [pos for row in puzzle[y_min : y_min+3] for pos in row[x_min : x_min+3]]
 
 def is_complete(puzzle):
     for row in puzzle:
@@ -27,26 +24,19 @@ def print_puzzle(puzzle):
         row_strs.append(row_str)
     puzzle_str = "\n".join(row_strs)
     print(puzzle_str)
-    
-def find_possibles(puzzle):
-    possibles = [[set() for x in range(9)] for y in range(9)]
-    for y, row in enumerate(possibles):
-        for x, pos in enumerate(row):
-            if puzzle[y][x] != 0:
-                possibles[y][x] = {puzzle[y][x]}
-                continue
-            visible_nums = set(get_row(puzzle, y) + get_col(puzzle, x) + get_square(puzzle, x, y))
-            possibles[y][x] = {n for n in range(1, 10)}.difference(visible_nums)
-    return possibles
 
 def sudoku(puzzle):
     """return the solved puzzle as a 2d array of 9 x 9"""
+    all_nums = {n for n in range(1, 10)}
     while not is_complete(puzzle):
-        possibles = find_possibles(puzzle)
-        for y, row in enumerate(possibles):
+        for y, row in enumerate(puzzle):
             for x, pos in enumerate(row):
-                if len(pos) == 1:
-                    puzzle[y][x] = list(pos)[0]
+                if pos != 0:
+                    continue
+                visible_nums = set(get_row(puzzle, y) + get_col(puzzle, x) + get_square(puzzle, x, y))
+                possible_nums = all_nums - visible_nums
+                if len(possible_nums) == 1:
+                    puzzle[y][x] = list(possible_nums)[0]
     return puzzle
 
 puzzle = [[5,3,0,0,7,0,0,0,0],
